c063f62c7a0623fd314357bb76d37ebd26e7840d
Christian Johannes <cajiky@gmail.com>
2024-05-04 15:39:25 -0400

    finished the front end functionality of the forum

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -11,15 +11,40 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     });
   });
 
-document.getElementById('contact-form').addEventListener('submit', function(e) {
-e.preventDefault();
-    // Validation code here
-    if (/* validation succeeds */) {
-      // Submit form or show success message
+// document.getElementById('contact-form').addEventListener('submit', function(e) {
+// e.preventDefault();
+//     // Validation code here
+//     if (/* validation succeeds */) {
+//       // Submit form or show success message
+//     } else {
+//       // Show error message
+//     }
+//   });
+
+  document.getElementById('contactForm').addEventListener('submit', function(event){
+    event.preventDefault();
+
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var message = document.getElementById('message').value;
+
+    if(name && email){
+      console.log("Name:", name);
+      console.log("Email:", email);
+      console.log("Message:", message);
+    
+      //Ajax Request
+
+      alert("Thank you for your submissions!");
+
     } else {
-      // Show error message
+
+      alert('Please fill out all the fields')
+
     }
-  });
+    alert('Thank you for your message');
+
+  })
 
   
 console.log('gia is a loving soulmate!');
